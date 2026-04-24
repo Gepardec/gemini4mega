871657a1de317b7cac5568e31720e50852eec4c9
jaroslav380 <jaroslav.romashchuk@gepardec.com>
2026-04-24 13:51:38 +0200

    add basic connection to LLM and rules

--- src/main/java/com/gepardec/rest/CheckingController.java
+++ src/main/java/com/gepardec/rest/CheckingController.java
@@ -1,16 +1,32 @@
 package com.gepardec.rest;
 
-import jakarta.ws.rs.GET;
-import jakarta.ws.rs.Path;
-import jakarta.ws.rs.Produces;
+import com.gepardec.llm.service.PromptService;
+import jakarta.inject.Inject;
+import jakarta.ws.rs.*;
 import jakarta.ws.rs.core.MediaType;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 @Path("/checking")
 public class CheckingController {
-    @GET
-    @Produces(MediaType.TEXT_PLAIN)
+    private static final Logger log = LoggerFactory.getLogger(CheckingController.class);
+    @Inject
+    PromptService promptService;
 
-    public String checking(String message){
-        return "";
+    @POST
+    @Produces(MediaType.APPLICATION_JSON)
+    @Consumes(MediaType.TEXT_PLAIN)
+    public String checking(String entries){
+        if (!entries.isEmpty()) {
+            try {
+                return promptService.prompt(entries);
+            }
+            catch (Exception e) {
+                log.error(String.valueOf(e));
+                return "Something went wrong";
+            }
+        } else {
+            return "No entries provided";
+        }
     }
 }
